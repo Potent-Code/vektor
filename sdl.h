c6bf39f291624e4be946b62f791aff8b689228d2
Ryan Lucchese <ryan.lucchese@gmail.com>
2011-05-14 22:37:27 -0600

    fixed sdl.h

--- sdl.h
+++ sdl.h
@@ -3,23 +3,22 @@
 
 #include "config.h"
 
-// linux
-#if HAVE_GL_GL_H
-	#include <GL/gl.h>
-#endif
-#if HAVE_GL_GLU_H
-	#include <GL/glu.h>
-#endif
-
-// darwin
 #if HAVE_OPENGL_GL_H
 	#include <OpenGL/gl.h>
-#endif
-#if HAVE_OPENGL_GLU_H
-	#include <OpenGL/glu.h>
+#else
+	#if HAVE_GL_GL_H
+		#include <GL/gl.h>
+	#endif
 #endif
 
-// everybody
+#if HAVE_OPENGL_GLU_H
+	#include <OpenGL/glu.h>
+#else
+	#if HAVE_GL_GLU_H
+		#include <GL/glu.h>
+	#endif
+#endif
+
 #include <SDL/SDL.h>
 
 #endif
