fb20c671eabacfa93ca50e499398f3200aa47b58
Ryan Lucchese <ryan.lucchese@gmail.com>
2011-05-14 22:13:10 -0600

    OpenGL autotools macro

--- sdl.h
+++ sdl.h
@@ -1,8 +1,25 @@
 #ifndef SDL_H
 #define SDL_H
 
-#include <GL/gl.h>
-#include <GL/glu.h>
+#include "config.h"
 
+// linux
+#if HAVE_GL_GL_H
+	#include <GL/gl.h>
+#endif
+#if HAVE_GL_GLU_H
+	#include <GL/glu.h>
+#endif
+
+// darwin
+#if HAVE_OPENGL_GL_H
+	#include <OpenGL/gl.h>
+#endif
+#if HAVE_OPENGL_GLU_H
+	#include <OpenGL/glu.h>
+#endif
+
+// everybody
 #include <SDL/SDL.h>
+
 #endif
