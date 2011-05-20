2899e7b1c0bcc8b7c0902bbfdf0ee7352d16828e
Ryan Lucchese <ryan.lucchese@gmail.com>
2011-05-20 01:41:34 -0600

    trying gethostbyname2()

--- src/net.c
+++ src/net.c
@@ -21,14 +21,14 @@ void init_network(void)
 		return;
 	}
 
-	//if((server = gethostbyname(host)) == NULL)
-	//{
-	//	perror("gethostbyname() failed");
-	//	return;
-	//}
+	if((server = gethostbyname2(host,AF_INET)) == NULL)
+	{
+		perror("gethostbyname2() failed");
+		return;
+	}
 	
-	//memcpy(&server_addr.sin_addr, server->h_addr_list[0], server->h_length);
-	inet_pton(AF_INET, "174.51.144.21",&server_addr.sin_addr.s_addr);
+	memcpy(&server_addr.sin_addr, server->h_addr_list[0], server->h_length);
+	//inet_pton(AF_INET, "174.51.144.21",&server_addr.sin_addr.s_addr);
 	server_addr.sin_family = PF_INET;
 	server_addr.sin_port = htons(port);
 	net_read.fd = sockfd;
