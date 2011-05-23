a3b7292d20a49a5c738e086b449734f7ecae7f1f
Ryan Lucchese <ryan.lucchese@gmail.com>
2011-05-23 03:24:22 -0600

    default window size switched to 1024x768

--- src/sdl_window.c
+++ src/sdl_window.c
@@ -76,11 +76,11 @@ void init_window(const char *title)
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 	SDL_WM_SetCaption(title, NULL);
 
-	surface = SDL_SetVideoMode(800,600,32,video_flags);
+	surface = SDL_SetVideoMode(1024,768,32,video_flags);
 
 	
 	init_scene();
-	resize(800,600);
+	resize(1024,768);
 
 	// this is a hack to enable input from the start
 	// something better should be done about this
