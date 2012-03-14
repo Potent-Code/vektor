41b6ac7657ed42c6c1c2fa1e7486b6c09b89f01f
Ryan Lucchese <ryan.lucchese@gmail.com>
2012-03-13 22:35:21 -0600

    fix frag shader mac

--- src/shaders/fs_simple_mac.fs
+++ src/shaders/fs_simple_mac.fs
@@ -1,11 +1,8 @@
 #version 120
 
-//precision highp float; // needed only for version 1.30
-
 varying  vec3 ex_Color;
-//varying vec4 out_Color;
 
 void main(void)
 {
-       out_Color = vec4(ex_Color,1.0);
+       gl_FragColor = vec4(ex_Color,1.0);
 }
