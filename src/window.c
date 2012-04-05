c22abf95ab7d0124a3f8a05651bdb91c2e418ba8
Ryan Lucchese <ryan.lucchese@write-c.com>
2012-04-04 22:56:22 -0600

    add window.c and window.h

---
+++ src/window.c
@@ -0,0 +1,10 @@
+/* application window
+ * by Ryan Lucchese
+ * April 4 2012 */
+
+#include "window.h"
+
+unsigned int window_width = 1024;
+unsigned int window_height = 768;
+unsigned int color_depth = 32;
+
---
+++ src/window.h
@@ -0,0 +1,13 @@
+/* application window
+ * by Ryan Lucchese
+ * April 4 2012 */
+
+#ifndef window_h
+#define window_h
+
+extern unsigned int window_width;
+extern unsigned int window_height;
+extern unsigned int color_depth;
+
+#endif
+
