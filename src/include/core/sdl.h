fbd80580606bd3902a3bcb5042273493347bb519
Ryan Lucchese <ryan.lucchese@write-c.com>
2015-03-22 19:27:24 -0600

    changed SDL includes to SDL2 includes

--- src/include/core/sdl.h
+++ src/include/core/sdl.h
@@ -7,10 +7,10 @@
 #ifndef vektor_in_h
 #define sdl_h
 
-#define GL_GLEXT_PROTOTYPES
-#define NO_SDL_GLEXT
-#include <SDL/SDL.h>
-#include <SDL/SDL_opengl.h>
+//#define GL_GLEXT_PROTOTYPES
+//#define NO_SDL_GLEXT
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 #endif // vektor_in_h
 
